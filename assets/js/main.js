0f7adaaa7334c4beaf8708a92480035640e14f45
SteffenKloempges <steffen.kloempges@gmail.com>
2021-03-09 14:02:17 +0100

    fixedFixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-fetch('https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyBKvcd0kTOrakKPJh6_fS3DrLdBHv2jbpM', { mode: "cors" })
-    .then(response => response.json())
-    .then(data => console.log(data))let input = document.getElementById("give-city")
 let result = document.getElementById("result")
 let submit = document.getElementById("submit")
 let sunUp = document.getElementById("sun-up")
@@ -31,12 +28,6 @@ submit.addEventListener("click", function (event) {
             let temp2 = (Math.floor(data.main.temp_max) - 273)
             let press = data['main']['pressure']
             let feuchtigkeit = data['main']['humidity']
-            let lottie = document.querySelector("#lottie-container");
-            // if (data.weather[0].description === "clear sky") {
-            //     lottie.innerHTML = `<lottie-player src="https://assets3.lottiefiles.com/packages/lf20_ms53nlfm.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>`;
-            // } else {
-            //     lottie.innerHTML = `<lottie-player src="https://assets3.lottiefiles.com/packages/lf20_ms53nlfm.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autop`;
-            // }
             let d = new Date()
             let tag = [
                 "Sonntag",
